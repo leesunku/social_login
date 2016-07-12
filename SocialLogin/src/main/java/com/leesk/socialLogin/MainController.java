bdaf2c3fccee85c496dec362f9867ef7701f4be5
devleesk <devleesk@devleesk-PC>
2016-07-12 13:43:44 +0900

    2016 - 07 - 12 1.1. 세션 유지 및 위조 방지용 상태 토큰 생성 by Sunku, Lee

--- SocialLogin/src/main/java/com/leesk/socialLogin/MainController.java
+++ SocialLogin/src/main/java/com/leesk/socialLogin/MainController.java
@@ -1,12 +1,30 @@
+/*
+*
+* made by Sunku, Lee
+* since - 2016.07.12
+* email - devleesk@daumsoft.com
+* 
+* 
+* 참조 사이트 : https://developers.naver.com/docs/login/web
+*/
 package com.leesk.socialLogin;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 public class MainController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index() {
-		
+	public String index(HttpSession session) {
+		session.setAttribute("state", generateState());
 		return "index";
 	}
 	
+	// randem state generate
+	private String generateState(){
+	    return new BigInteger(130, new SecureRandom()).toString(32);
+	}
 }
--- SocialLogin/src/main/webapp/WEB-INF/views/index.jsp
+++ SocialLogin/src/main/webapp/WEB-INF/views/index.jsp
@@ -1,3 +1,4 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -5,8 +6,18 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
+<script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.12.2.min.js">
+</script>
+<script type="text/javascript">
+$(function(){
+	$("#loginBte").click(function(){
+	location.href = "login";
+	});
+});
+</script>
 </head>
 <body>
 <h1>소셜 로그인 메인 페이지</h1>
+<button id="loginBte">네이버 아이디로 로그인</button>
 </body>
 </html>
